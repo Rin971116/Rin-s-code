b075020acdecec8013bff69d29e3274d8e74bd11
Rin971116 <gummy789123@gmail.com>
2024-04-29 17:40:17 +0800

    modify print to utf-8

--- src/com/project/project1/GoAccountCalculator.java
+++ src/com/project/project1/GoAccountCalculator.java
@@ -7,7 +7,7 @@ class AccountCalculator{
 
 	int times = 0;
 	int allMoney = 0;
-	//?H?U????i??A?}?C???äD
+	//以下為宣告動態陣列的方式
 	ArrayList<Integer> details = new ArrayList<Integer>();
 	ArrayList<String> directions = new ArrayList<String>();
 
@@ -15,14 +15,14 @@ class AccountCalculator{
 		allMoney += money;
 		details.add(money);
 		times++;
-		System.out.println("?w?s?J"+ money +"???A??e?b??l?B??:"+ allMoney +"??");
+		System.out.println("已存入"+ money +"元，目前帳戶餘額為:"+ allMoney +"元");
 	}
 
 	void minusMoney(int money){
 		allMoney -= money;
 		details.add(-money);
 		times++;
-		System.out.println("?w????"+ money +"???A??e?b??l?B??:"+ allMoney +"??");
+		System.out.println("已扣款"+ money +"元，目前帳戶餘額為:"+ allMoney +"元");
 	}
 
 	void storeDirections(String directions){
@@ -30,7 +30,7 @@ class AccountCalculator{
 	}
 	
 	void showDetails(){
-		System.out.println("?b??l?B\t??????B\t??    ??");
+		System.out.println("帳戶餘額\t收支金額\t說    明");
 		if(times != 0){
 			for(int index = 0; index <= times-1; index++){
 				System.out.println(allMoney + "\t\t" + details.get(index) + "\t\t" + directions.get(index));
@@ -50,36 +50,36 @@ class GoAccountCalculator{
 	
 	loopOption:while(true){					
 					System.out.println("----------------------------------------\n");
-					System.out.println("1.???????");
-					System.out.println("2.?n?O???J");
-					System.out.println("3.?n?O??X");
-					System.out.println("4.?h?X\n");
+					System.out.println("1.收支明細");
+					System.out.println("2.登記收入");
+					System.out.println("3.登記支出");
+					System.out.println("4.退出\n");
 					System.out.println("----------------------------------------\n");
-					System.out.println("???J1-4:");
+					System.out.println("請輸入1-4:");
 					
 					String option = sc.next();
 					
 					switch(option){
 						case "1": 
-							System.out.println("----------------?b?????----------------");
+							System.out.println("----------------帳戶紀錄----------------");
 							ac1.showDetails();
 							break;
 						case "2":
-							System.out.println("???J?A???J?????B:");
+							System.out.println("請輸入你收入的金額:");
 							ac1.addMoney(sc.nextInt());
-							System.out.println("???J????:");
+							System.out.println("請輸入說明:");
 							ac1.storeDirections(sc.next());
 							break;
 						case "3": 
-							System.out.println("???J?A??X?????B:");
+							System.out.println("請輸入你支出的金額:");
 							ac1.minusMoney(sc.nextInt());
-							System.out.println("???J????:");
+							System.out.println("請輸入說明:");
 							ac1.storeDirections(sc.next());
 							break;
 						case "4": 
-							System.out.println("???b?h?X...");
+							System.out.println("正在退出...");
 							break loopOption;
-						default: System.out.print("??J???~");
+						default: System.out.print("輸入錯誤");
 							break;
 					}
 				}
